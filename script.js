0a77963ee6e6010384cce115be1e90f4f91f4312
Ofridani <104002869+Ofridani@users.noreply.github.com>
2024-12-31 15:15:08 +0200

    Created repo

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice()
+{
+    const choice = Math.floor(Math.random() * 3);
+    switch (choice)
+    {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
