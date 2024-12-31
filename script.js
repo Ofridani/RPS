8a6e951528691f84a26d93d0c31caa17840d759f
Ofridani <104002869+Ofridani@users.noreply.github.com>
2024-12-31 15:32:13 +0200

    Created getHumanChoice

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function getComputerChoice()
-{
+function getComputerChoice(){
     const choice = Math.floor(Math.random() * 3);
     switch (choice)
     {
@@ -8,6 +7,23 @@ function getComputerChoice()
         case 1:
             return "Paper";
         case 2:
+            return "Scissors";
+    }
+}
+
+function getHumanChoice(){
+    let choice = prompt("Enter your choice:\nR for Rock, P for Paper, S for Scissors").toUpperCase();
+    while (choice != "R" && choice != "P" && choice != "S"){
+        alert("Invalid Choice!");
+        choice = prompt("Enter your choice:\nR for Rock, P for Paper, S for Scissors").toUpperCase();
+    }
+    switch (choice)
+    {
+        case "R":
+            return "Rock";
+        case "P":
+            return "Paper";
+        case "S":
             return "Scissors";
     }
 }
