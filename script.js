c9da28f98baba751053f96b76466371d2e8a714b
Ofridani <104002869+Ofridani@users.noreply.github.com>
2025-01-08 01:20:21 +0200

    Created start of UI

--- script.js
+++ script.js
@@ -1,60 +1,65 @@
-function getComputerChoice(){
-    const choice = Math.floor(Math.random() * 3);
-    switch (choice)
-    {
-        case 0:
-            return "Rock";
-        case 1:
-            return "Paper";
-        case 2:
-            return "Scissors";
-    }
-}
+class Game{
 
-function getHumanChoice(){
-    let choice = prompt("Enter your choice:\nR for Rock, P for Paper, S for Scissors").toUpperCase();
-    while (choice != "R" && choice != "P" && choice != "S"){
-        alert("Invalid Choice!");
-        choice = prompt("Enter your choice:\nR for Rock, P for Paper, S for Scissors").toUpperCase();
+    constructor(){
+        this.humanScore = 0;
+        this.computerScore = 0;
+        this.rounds = 0;
     }
-    switch (choice)
-    {
-        case "R":
-            return "Rock";
-        case "P":
-            return "Paper";
-        case "S":
-            return "Scissors";
-    }
-}
 
-function playGame() // Play a game of 5 rounds
-{
-    let humanScore = 0, computerScore=0;
-    function playRound(humanChoice, computerChoice){
-        if (humanChoice == computerChoice){
+    getComputerChoice(){
+        const choice = Math.floor(Math.random() * 3);
+        switch (choice)
+        {
+            case 0:
+                return "Rock";
+            case 1:
+                return "Paper";
+            case 2:
+                return "Scissors";
+        }
+    }
+
+    updateBoard(){
+        const humanScoreElement = document.querySelector("#humanScore");
+        const computerScoreElement = document.querySelector("#computerScore");
+        humanScoreElement.textContent = `You: ${this.humanScore}`;
+        computerScoreElement.textContent = `Computer: ${this.computerScore}`;
+        if (this.rounds >= 5){
+            const container = document.querySelector(".container");
+            const winnerElement = document.createElement("div");
+            winnerElement.textContent = this.humanScore > this.computerScore ? "You win!" : "You lose!";
+            container.appendChild(winnerElement);
+        }
+    }
+
+    resetBoard(){
+        
+    }
+
+    playRound(humanChoice){
+        let computerChoice = this.getComputerChoice();
+        this.rounds++;
+        if (humanChoice === computerChoice){
+            this.updateBoard();
             return;
         }
         switch(humanChoice[0]+computerChoice[0]){
             case ("RS"):
-                humanScore++;
-                return;
             case ("PR"):
-                humanScore++;
-                return;
             case ("SP"):
-                humanScore++;
-                return;
+            {
+                this.humanScore++;
+                break;
+            }
             default:
-                computerScore++;
-                return;
+            {
+                this.computerScore++;
+            }
         }
+        this.updateBoard();
     }
-
-    for (let i = 0; i < 5; i++){
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    humanScore > computerScore ? alert("You win!") : alert("Computer Wins...")
 }
 
-playGame()
+game = new Game();
+const btns = document.querySelectorAll("button");
+btns.forEach((btn) => btn.addEventListener("click", () => game.playRound(btn.id)));
